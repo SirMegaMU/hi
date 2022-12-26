69be25136c2d66dc1df06622bb9d4690ee667cdf
MegaMU <sirmegamu@outlook.com>
2022-12-26 23:33:35 +0800

    modified:   hi.sh

--- hi.sh
+++ hi.sh
@@ -1,9 +1,8 @@
 #!/usr/bash 
 if (($# > 0 )); then
-		if [[ $* =~ "gitpull" ]]; then
 
-				ssh -T git@github.com 2>/dev/null 
-				
+		if [[ $* =~ "gitpull" ]]; then
+				ssh -T git@github.com 2>/dev/null 			
 				for gitrepo in ~/Documents/GitHUb/*; do
 						cd $gitrepo  
 						echo -n in ; echo -e "\e[34m \c" ; echo -n $gitrepo  ; echo -e "\e[0m"
@@ -13,13 +12,19 @@ if (($# > 0 )); then
 		fi
 
 		if [[ $* =~ "ip" ]]; then
-
 				GateWay=(`ip route | grep default | grep -oP '[0-9]*\.[0-9]*\.[0-9]*\.[0-9]*'`)
 				ExtIP=(`bash ~/.tooys/ip_addr.sh`) 
-
 				echo -n "Ext IP Addr : " ; echo -e "\e[34m\c" ; echo -n $ExtIP   ; echo -e "\e[0m"
 				echo -n "GateWay     : " ; echo -e "\e[34m\c" ; echo -n $GateWay ; echo -e "\e[0m"
+		fi
 
+		if [[ $* =~ "startup" ]]; then
+			StartUpTime=(`systemd-analyze time | grep -oP '[0-9]+.[0-9]*s'`)
+			echo -n "Startup finished in :" ; echo -e "\e[34m\c" ; echo -n ${StartUpTime[4]} ;echo -e "\e[0m"
+			echo -n "┝━ firmware   :" ; echo -e "\e[34m\c" ; echo -n ${StartUpTime[0]} ;echo -e "\e[0m"
+			echo -n "┝━ loader     :" ; echo -e "\e[34m\c" ; echo -n ${StartUpTime[1]} ;echo -e "\e[0m"
+			echo -n "┝━ kernel     :" ; echo -e "\e[34m\c" ; echo -n ${StartUpTime[2]} ;echo -e "\e[0m"
+			echo -n "┕━ userspace  :" ; echo -e "\e[34m\c" ; echo -n ${StartUpTime[3]} ;echo -e "\e[0m"
 		fi
 		
 else 
