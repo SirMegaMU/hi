ebd726ab4e8c5ec1da1275eab0f692f970325bcb
MegaMU <sirmegamu@outlook.com>
2023-01-03 21:24:22 +0800

    modified:   README.md 	new file:   conf.json 	modified:   hi.sh

--- hi.sh
+++ hi.sh
@@ -1,18 +1,22 @@
 #!/usr/bash 
 if (($# > 0 )); then
 
-	# echo ${0/%hi.sh/*} 
+	# echo ${0/%hi.sh/source}
 	for sourcedir in ${0/%hi.sh/source/*} ; do
+		# echo $sourcedir
 		if [[ -d $sourcedir ]]; then
-			cd $sourcedir
-			if [[ -e main.sh ]]; then
-				 bash ./main.sh $* 
-			fi
-			cd ~
+			if [[ $sourcedir =~ $1 ]]; then
+			  # echo ${sourcedir/main.sh}
+			  if [[ -e $sourcedir/main.sh ]]; then
+				  bash ${sourcedir}/main.sh $*
+			    break
+			  fi
+			fi 
 		fi
 	done
 	
 else 
 	echo `date`
-	figlet MegaMU
+	username=(`cat ~/.hi/conf.json | grep "user" | grep -oP '"user" : "(.*)"?'`) ; username=${username[2]:1:-1}
+	figlet $username
 fi
